9ba007191959b3f2b2cbc9ea9eb1c76dc8024678
Hithesh Gowda <gowdahithesh817@gmail.com>
2025-07-26 01:26:56 +0200

    four

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ app = Flask(__name__)
 
 @app.route("/info")
 def lwinfo():
-        return "Hello there I am Hithesh"
+        return "Hello there I am Hithesh gowda"
 
 @app.route("/phone")
 def lwphone():
         return "123456789"
 
-app.run(host="0.0.0.0")
+app.run(host="0.0.0.0", port=5001)
 
 
