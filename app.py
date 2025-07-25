c278a0f9486440303c90762cc7e5a65699066766
Hithesh Gowda <gowdahithesh817@gmail.com>
2025-07-25 17:09:45 +0200

    first

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/info")
+def lwinfo():
+        return "Hello there I am Hithesh"
+
+@app.route("/phone")
+def lwphone():
+        return "123456789"
+
+app.run(host="0.0.0.0")
+
+
